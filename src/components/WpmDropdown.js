2db6b352c82cf3427d489e45e11d4502f1759ba0
Simon Egersand <s.egersand@gmail.com>
2018-05-28 18:46:51 +0200

    Make dropdown look nice

--- src/components/WpmDropdown.js
+++ src/components/WpmDropdown.js
@@ -2,7 +2,7 @@ import {createElement as f} from 'react'
 
 const wpmSpeeds = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 700]
 
-export default ({onChange, wpm}) => (
+export default ({onChange}) => (
   f('select', {id: 'wpm-dropdown', defaultValue: wpmSpeeds[0], onChange},
     wpmSpeeds.map(speed => f('option', {value: speed, key: speed}, `${speed} wpm`))
   )
--- src/containers/Input.js
+++ src/containers/Input.js
@@ -1,5 +1,5 @@
-import { createElement as f } from 'react'
-import { connect } from 'react-redux'
+import {createElement as f} from 'react'
+import {connect} from 'react-redux'
 import Textarea from '../components/Textarea'
 import {inputOnChange, setWordTickerRunning, wpmOnChange} from '../actions'
 import WpmDropdown from '../components/WpmDropdown'
@@ -11,29 +11,29 @@ const mapStateToProps = state => ({
   required: state.reducer.inputRequired
 })
 
-const handleSubmit = ({ event, dispatch }) => {
+const handleSubmit = ({event, dispatch}) => {
   // Do not re-load page
   event.preventDefault()
   dispatch(setWordTickerRunning(true))
 }
 
-const handleChange = ({ event, dispatch }) =>
+const handleChange = ({event, dispatch}) =>
   dispatch(inputOnChange(event.target.value))
 
-const handleWpmChange = ({ event, dispatch }) =>
+const handleWpmChange = ({event, dispatch}) =>
   dispatch(wpmOnChange(event.target.value))
 
-const clearTextarea = ({ dispatch }) => dispatch(inputOnChange(''))
+const clearTextarea = ({dispatch}) => dispatch(inputOnChange(''))
 
-const Input = ({ dispatch, autoFocus, placeholder, required, userInput, wpm }) => {
+const Input = ({dispatch, autoFocus, placeholder, required, userInput, wpm}) => {
   return (
-    f('div', { id: 'inner' },
+    f('div', {id: 'inner'},
       f('form', {
-        onSubmit: event => handleSubmit({ event, dispatch })
+        onSubmit: event => handleSubmit({event, dispatch})
       },
       f(Textarea, {
         autoFocus,
-        onChange: event => handleChange({ event, dispatch }),
+        onChange: event => handleChange({event, dispatch}),
         placeholder,
         required,
         value: userInput
@@ -47,10 +47,10 @@ const Input = ({ dispatch, autoFocus, placeholder, required, userInput, wpm }) =
         id: 'btn-clear',
         className: 'btn',
         type: 'button',
-        onClick: () => clearTextarea({ dispatch })
-      }, 'Clear')
-      ),
-      f(WpmDropdown, { wpm, onChange: event => handleWpmChange({ event, dispatch }) }, null)
+        onClick: () => clearTextarea({dispatch})
+      }, 'Clear'),
+      f(WpmDropdown, {wpm, onChange: event => handleWpmChange({event, dispatch})}, null)
+      )
     )
   )
 }
