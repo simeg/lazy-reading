7e66bfe6241d6f8ffcd5c0e9c9bc3f35be2a9351
Simon Egersand <s.egersand@gmail.com>
2018-05-28 23:53:01 +0200

    Make highlighted char in output be statically placed
    This is the real trick to lazy reading, not having to move your eyes one
    bit. The pattern of what character to highlight is the exact same that
    the "actual" tool is using (spritzinc).

--- src/components/WordTicker.js
+++ src/components/WordTicker.js
@@ -30,9 +30,33 @@ export default class WordTicker extends Component {
     clearInterval(this.interval);
   }
 
-  render () {
-    return (
-      f('div', { id: 'letter-ticker' }, this.state.word)
-    )
+  getHighlightIndex(word) {
+    switch (word.length) {
+      case 1:
+        return 0;
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        return 1;
+      default:
+        return 2;
+    }
+  }
+
+  renderWord(word) {
+    if (!word) return null;
+    const index = this.getHighlightIndex(word);
+
+    // TODO: This could be cleaned up
+    const pre = f("div", { id: "pre" }, word.slice(0, index));
+    const highlight = f("div", { id: "highlight" }, word.charAt(index));
+    const post = f("div", { id: "post" }, word.slice(index + 1, word.length));
+
+    return f("div", { id: "word-container" }, pre, highlight, post);
+  }
+
+  render() {
+    return f("div", { id: "letter-ticker" }, this.renderWord(this.state.word));
   }
 }
